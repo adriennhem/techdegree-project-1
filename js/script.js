c438cd34d1249acd5c91a3e067cd23d2fe90fbcc
adrien nhem <adrien.nhem@gmail.com>
2020-04-11 15:37:49 +0100

    add starter files

--- js/script.js
+++ js/script.js
@@ -35,13 +35,11 @@ let quotes = [
 ];
 
 // create random quote object builder.
+function getRandomQuote() {
+    // Code to get quote from array
 
+};
 
-
-// “Any fool can write code that a computer can understand. Good programmers write code that humans can understand.” – Martin Fowler
-// “First, solve the problem. Then, write the code.” – John Johnson
-// “Experience is the name everyone gives to their mistakes.” – Oscar Wilde
-// “ In order to be irreplaceable, one must always be different” – Coco Chanel
-// “Java is to JavaScript what car is to Carpet.” – Chris Heilmann
-
-// 
+function printQuote() {
+    // Code to print the quote
+};
