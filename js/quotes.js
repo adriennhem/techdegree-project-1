f3b3352625b5f1aa6d07e64bbb020fbd5e4023be
adrien nhem <adrien.nhem@gmail.com>
2020-04-11 15:06:03 +0100

    first commit

---
+++ js/quotes.js
@@ -0,0 +1,47 @@
+// Create an array of 5 objects named quotes
+// Properties: quote, source, citation, year
+
+let quotes = [
+    {
+        quote: "Women love us for our defects. If we have enough of them, they will forgive us everything, even our intellects.",
+        source: "Oscar Wilde",
+        citation: "The Picture of Dorian Gray",
+        year: 1891
+    },
+    {
+        quote: "An honest tale speeds best, being plainly told.",
+        source: "William Shakespeare",
+        citation: "King Richard III",
+        year: 1597
+    },
+    {
+        quote: "Every science begins as philosophy and ends as art.",
+        source: "Will Durant",
+        citation: "The Story of Philosophy",
+        year: 1926
+    },
+    {
+        quote: "We live as we dream - alone.",
+        source: "Joseph Conrad",
+        citation: "Heart of Darkness",
+        year: 1899 
+    },
+    {
+        quote: "Hard work's a good distraction.",
+        source: "Scott Westerfeld",
+        citation: "Uglies",
+        year: 2005
+    }
+];
+
+// create random quote object builder.
+
+
+
+// “Any fool can write code that a computer can understand. Good programmers write code that humans can understand.” – Martin Fowler
+// “First, solve the problem. Then, write the code.” – John Johnson
+// “Experience is the name everyone gives to their mistakes.” – Oscar Wilde
+// “ In order to be irreplaceable, one must always be different” – Coco Chanel
+// “Java is to JavaScript what car is to Carpet.” – Chris Heilmann
+
+// 
