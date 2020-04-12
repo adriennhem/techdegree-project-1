2b3d39203a5c6c0f07db3dae02eea246f00b8e16
adrien nhem <adrien.nhem@gmail.com>
2020-04-12 10:08:36 +0100

    test GIF on README

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Adrien Nhem - Techdegree Project 1
 
 ## Build a random quote generator
+
+![](https://media.giphy.com/media/kfdWmcO5s7dqZVpW7F/giphy.gif | width=50)
+
--- js/script.js
+++ js/script.js
@@ -1,47 +1,54 @@
-// Create an array of 5 objects named quotes
-// Properties: quote, source, citation, year
-let quotes = [
+/******************************************
+Treehouse FSJS Techdegree:
+project 1 - A Random Quote Generator
+******************************************/
+
+const quotes = [
     {
         quote: "Women love us for our defects. If we have enough of them, they will forgive us everything, even our intellects.",
         source: "Oscar Wilde",
         citation: "The Picture of Dorian Gray",
-        year: 1891
+        year: 1891, 
+        tags: ["books", "classic", "old"]
     },
     {
         quote: "An honest tale speeds best, being plainly told.",
         source: "William Shakespeare",
         citation: "King Richard III",
-        year: 1597
+        year: 1597,
+        tags: ["theatre", "classic", "old"]
     },
     {
         quote: "Every science begins as philosophy and ends as art.",
         source: "Will Durant",
         citation: "The Story of Philosophy",
-        year: 1926
+        year: 1926,
+        tags: []
     },
     {
         quote: "We live as we dream - alone.",
         source: "Joseph Conrad",
         citation: "Heart of Darkness",
-        year: 1899 
+        year: 1899,
+        tags: ["heart", "books"] 
     },
     {
         quote: "Hard work's a good distraction.",
         source: "Scott Westerfeld",
         citation: "Uglies",
-        year: 2005
+        year: 2005,
+        tags: []
     }
 ];
 
 // create random quote object builder.
 function getRandomQuote() {
-    // Code to get quote from array
     let randomNumber =  Math.floor( Math.random() * Math.floor(quotes.length));
     return quotes[randomNumber];
 };
 
+// Function printQuote
 function printQuote() {
-    // Code to print the quote
     let randomQuote = getRandomQuote(); 
     let htmlOutput = `<p class="quote">${randomQuote.quote}</p>
                       <p class="source">${randomQuote.source}`
@@ -52,10 +59,32 @@ function printQuote() {
     if (randomQuote.year != '') {
         htmlOutput += `<span class="year">${randomQuote.year}</span>`
     }
+    if (randomQuote.tags.length > 0) {
+        htmlOutput += `<br>`
+        for (i = 0; i < randomQuote.tags.length; i++ ) {
+            htmlOutput += `<span class="tags">#${randomQuote.tags[i]}</span> `
+        }
+    }
 
     htmlOutput += `</p>`;
     document.getElementById('quote-box').innerHTML = htmlOutput; 
+    updateBackgroundColor();
 
 };
 
-printQuote();
+// Change the color of the background
+// Inspired by https://css-tricks.com/snippets/javascript/random-hex-color/
+
+function updateBackgroundColor() {
+    const randomColor =  Math.floor(Math.random()*16777215).toString(16);
+    document.body.style.backgroundColor = `#${randomColor}`;
+};
+
+// Change the quote when the button is clicked
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
+
+// Auto-refreshes quotes
+setInterval(printQuote, 10000);
+
+// Just need to document and then submit for review.
+// Update the readme.md maybe with a GIF. 
